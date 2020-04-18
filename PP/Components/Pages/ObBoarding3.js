560cacb8a7f2aaafee9a61c947dca1acc0761c6b
kate2797 <katka.zaprazna@icloud.com>
2020-04-18 19:39:20 +0200

    onboarding finished

---
+++ PP/Components/Pages/ObBoarding3.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Image, StyleSheet, Text, View, Button } from 'react-native';
+import logo from './assets/logo.png';
+
+
+export default function OnBoardingThree() {
+  return (
+
+    <View style={styles.container}>
+      <Image source={logo} style={{ width: 100, marginBottom: 10 }}/>
+      <Text style={styles.headings}>Seek help</Text>
+      <Text style={styles.textBox}>Your health matters as well and we offer you a full support with mental health tips.</Text>
+
+      <View style={styles.dots}>
+        <View style={styles.circle}></View>
+        <View style={styles.circle}></View>
+        <View style={styles.circleFirst}></View>
+      </View>
+
+      <View style={styles.btn}>
+        <Button title="Next" color='#58ACA8' />
+      </View>
+
+    </View>
+
+
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+      textAlign: 'center',
+      paddingLeft: 37,
+      paddingRight: 38,
+    },
+
+    textBox: {
+      textAlign: 'center',
+      fontSize: 18,
+    },
+
+    headings: {
+      fontSize: 23,
+      paddingBottom: 6,
+  },
+
+  circle: {
+    width: 9,
+    height: 9,
+    borderRadius: 100/2,
+    backgroundColor: '#C4C4C4',
+    margin: 3,
+    marginTop: 45,
+  },
+
+  circleFirst: {
+    width: 9,
+    height: 9,
+    borderRadius: 100/2,
+    backgroundColor: '#58ACA8',
+    margin: 3,
+    marginTop: 45,
+  },
+
+  dots: {
+    flexDirection: 'row',
+  },
+
+  btn: {
+    marginTop: 20
+  }
+
+
+  });
